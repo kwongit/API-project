137d2d078aa949f499040a410c94baf420d3fc23
kevin <kevinwon30@gmail.com>
2023-08-06 14:48:18 -0700

    created review image migration, model, seed

---
+++ backend/db/models/reviewimage.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class ReviewImage extends Model {
+    static associate(models) {
+      ReviewImage.belongsTo(models.Review, {
+        foreignKey: "reviewId",
+      });
+    }
+  }
+  ReviewImage.init(
+    {
+      reviewId: {
+        type: DataTypes.INTEGER,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "ReviewImage",
+    }
+  );
+  return ReviewImage;
+};
