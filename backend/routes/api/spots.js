fbf1dc86f89d42491a0a7be2b18a65e05f39bae7
kevin <kevinwon30@gmail.com>
2023-08-07 21:25:19 -0700

    WIP: get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const { Spot } = require("../../db/models");
 
 const router = express.Router();
+
+// Get all spots
+router.get("/", async (req, res) => {
+  const spots = await Spot.findAll();
+
+  return res.status(200).json({
+    Spots: spots,
+  });
+});
 
 module.exports = router;
