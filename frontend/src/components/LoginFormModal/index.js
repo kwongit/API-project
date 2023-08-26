4e86bef17d6a5de18b9707775805767251b6577f
kevin <kevinwon30@gmail.com>
2023-08-25 19:32:32 -0700

    corrected login error handling message

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -9,30 +9,34 @@ function LoginFormModal() {
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [loginError, setLoginError] = useState("");
   const { closeModal } = useModal();
 
   useEffect(() => {
     const errors = {};
-
     if (credential.length < 4) {
       errors.credential = "Username must be 4 characters or more";
     }
     if (password.length < 6) {
       errors.password = "Password must be 6 characters or more";
     }
-
     setErrors(errors);
   }, [credential, password]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors({});
+    setLoginError("");
+
     return dispatch(sessionActions.login({ credential, password }))
       .then(closeModal)
       .catch(async (res) => {
         const data = await res.json();
         if (data && data.errors) {
+          // console.log("data", data);
+          // console.log("data errors", data.errors);
           setErrors(data.errors);
+          setLoginError("The provided credentials were invalid");
         }
       });
   };
@@ -59,10 +63,16 @@ function LoginFormModal() {
             required
           />
         </label>
+
         {errors.credential && credential.length > 0 && (
-          <p>{errors.credential}</p>
+          <p className="error-message">{errors.credential}</p>
         )}
-        {errors.password && password.length > 0 && <p>{errors.password}</p>}
+        {errors.password && password.length > 0 && (
+          <p className="error-message">{errors.password}</p>
+        )}
+
+        {loginError && <p className="error-message">{loginError}</p>}
+
         <button type="submit" disabled={Object.keys(errors).length > 0}>
           Log In
         </button>
