9094469c836f57ebb51a53a0cc47c806803c6ec1
kevin <kevinwon30@gmail.com>
2023-08-27 07:48:45 -0700

    wip signup modal

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { NavLink, useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
@@ -15,7 +14,31 @@ function SignupFormModal() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
-  const history = useHistory();
+
+  useEffect(() => {
+    const errors = {};
+
+    if (firstName.length < 1) {
+      errors.firstName = "";
+    }
+    if (lastName.length < 1) {
+      errors.lastName = "";
+    }
+    if (email.length < 1) {
+      errors.email = "";
+    }
+    if (username.length < 4) {
+      errors.username = "";
+    }
+    if (password.length < 6) {
+      errors.password = "";
+    }
+    if (confirmPassword.length < 6) {
+      errors.confirmPassword = "";
+    }
+
+    setErrors(errors);
+  }, [email, username, firstName, lastName, confirmPassword, password]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -44,85 +67,89 @@ function SignupFormModal() {
     });
   };
 
-  const handleLogoClick = () => {
-    history.push("/");
-  };
-
   return (
-    <>
-      {/* <NavLink exact to="/">
-        <img
-          className="logo"
-          src="../icon/logo.png"
-          alt="logo"
-          onClick={handleLogoClick}
-        />
-      </NavLink> */}
+    <div className="signup-container">
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          First Name
           <input
             type="text"
             value={firstName}
+            placeholder="First Name"
             onChange={(e) => setFirstName(e.target.value)}
             required
           />
         </label>
-        {errors.firstName && <p>{errors.firstName}</p>}
+        {errors.firstName && (
+          <p className="on-submit-errors">{errors.firstName}</p>
+        )}
         <label>
-          Last Name
           <input
             type="text"
             value={lastName}
+            placeholder="Last Name"
             onChange={(e) => setLastName(e.target.value)}
             required
           />
         </label>
-        {errors.lastName && <p>{errors.lastName}</p>}
+        {errors.lastName && (
+          <p className="on-submit-errors">{errors.lastName}</p>
+        )}
         <label>
-          Password
+          <input
+            type="text"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        {errors.email && <p className="on-submit-errors">{errors.email}</p>}
+        <label>
+          <input
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        {errors.username && (
+          <p className="on-submit-errors">{errors.username}</p>
+        )}
+        <label>
           <input
             type="password"
             value={password}
+            placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </label>
-        {errors.password && <p>{errors.password}</p>}
+        {errors.password && (
+          <p className="on-submit-errors">{errors.password}</p>
+        )}
         <label>
-          Confirm Password
           <input
             type="password"
             value={confirmPassword}
+            placeholder="Confirm Password"
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
         </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit">Sign Up</button>
+        {errors.confirmPassword && (
+          <p className="on-submit-errors">{errors.confirmPassword}</p>
+        )}
+        <button
+          className="submit-button"
+          type="submit"
+          disabled={Object.keys(errors).length > 0}
+        >
+          Sign Up
+        </button>
       </form>
-    </>
+    </div>
   );
 }
 
